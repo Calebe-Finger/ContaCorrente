acf01e7735f2e71e79d27e1df72dcbb48083b200
Calebe <calebepfinger@gmail.com>
2025-04-24 20:13:45 -0300

    Implementadas Movimentações e Extratos

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,12 +1,25 @@
 ﻿
 namespace ContaCorrente.ConsoleApp
 {
+    public class Movimentacao
+    {
+        public decimal valor;
+        public string tipo;
+
+        public string LerMovimentacao()
+        {
+            return $"{tipo} - {valor.ToString("C2")}";
+        }
+    }
+
     public class ContaCorrente
     {
         //atributos
         public decimal saldo;
         public int numero;
         public decimal limite;
+        public Movimentacao[] movimentacoes;
+        int contadorMovimentacoes = 0;
 
         //métodos
         public void Sacar(decimal quantia)
@@ -14,12 +27,26 @@ namespace ContaCorrente.ConsoleApp
             if (quantia < saldo + limite)
             {
                 saldo -= quantia;
+
+                Movimentacao novaMovimentacao = new Movimentacao();
+                novaMovimentacao.valor = quantia;
+                novaMovimentacao.tipo = "Débito";
+
+                movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+                contadorMovimentacoes++;
             }
         }
 
         public void Depositar(decimal quantia)
         {
             saldo += quantia;
+
+            Movimentacao novaMovimentacao = new Movimentacao();
+            novaMovimentacao.valor = quantia;
+            novaMovimentacao.tipo = "Crédito";
+
+            movimentacoes[contadorMovimentacoes] = novaMovimentacao;
+            contadorMovimentacoes++;
         }
 
         internal void TransferirPara(ContaCorrente destinatario, decimal quantia)
@@ -28,6 +55,22 @@ namespace ContaCorrente.ConsoleApp
 
             this.Sacar(quantia);
         }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine("O extrato da conta #" + this.numero);
+
+            Console.WriteLine("Saldo " + this.saldo);
+
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                Movimentacao movimentacaoAtual = movimentacoes [i];
+
+                if (movimentacaoAtual != null)
+                {
+                    Console.WriteLine(movimentacaoAtual.LerMovimentacao);
+                }
+            }
     }
 
     internal class Program
@@ -38,6 +81,7 @@ namespace ContaCorrente.ConsoleApp
             conta1.saldo = 1000;
             conta1.numero = 12;
             conta1.limite = 0;
+            conta1.movimentacoes = new Movimentacao[100];
 
             conta1.Sacar(200);
             conta1.Depositar(300);
@@ -48,6 +92,7 @@ namespace ContaCorrente.ConsoleApp
             conta2.saldo = 300;
             conta2.numero = 14;
             conta2.limite = 0;
+            conta2.movimentacoes = new Movimentacao[100];
 
             conta1.TransferirPara(conta2, 400);
 
